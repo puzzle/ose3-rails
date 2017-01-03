d324793cba8be2c609fa8d969daf82f80cc2aca1
Pascal Simon <simon@puzzle.ch>
2017-01-03 14:57:04 +0100

    sphinx indexer: configurable time interval

--- src/rails/sphinx/_context/s2i/run-sphinx.sh
+++ src/rails/sphinx/_context/s2i/run-sphinx.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+if [ -z $SPHINX_INTERVAL_TIME ]
+then
+  $SPHINX_INTERVAL_TIME=3600 # 10mins
+fi
+
 function scheduled_indexing {
   while :
   do
+    echo 'Starting indexer ...'
     /usr/bin/indexer --config config/production.sphinx.conf --all --rotate
-    sleep 3600
+    echo '... indexing done'
+    sleep $SPHINX_INTERVAL_TIME
   done
 }
 
@@ -13,7 +20,6 @@ bundle exec rake ts:configure
 
 # create initial index
 bundle exec rake ts:index
-
 
 # start indexer and sphinx daemon
 scheduled_indexing & \
