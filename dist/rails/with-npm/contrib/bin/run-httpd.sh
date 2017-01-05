37c4d80e3219935b5bcdde5b28980f62a50bcec9
Severin Räz <raez@puzzle.ch>
2017-01-05 16:17:56 +0100

    Join output of apache and rails to stdout

--- dist/rails/with-npm/contrib/bin/run-httpd.sh
+++ dist/rails/with-npm/contrib/bin/run-httpd.sh
@@ -2,13 +2,24 @@
 
 rm -rf $APACHE_PID_FILE
 
-# use pipe due to error in Docker 1.8
-mkfifo -m 777 /tmp/logpipe
-cat < /tmp/logpipe 1>&2 &
+# Set up log pipes in order to join all logs' outputs to stdout
+mkfifo -m 777 /tmp/log_pipe_apache
+mkfifo -m 777 /tmp/log_pipe_rails
+
+cat < /tmp/log_pipe_apache 2>&1 | sed 's/^/apache: /' &
+
+rm -rf /opt/app-root/src/log/production.log
+ln -s /tmp/log_pipe_rails /opt/app-root/src/log/production.log
+# Loop forces reopening of file after EOFs
+( while true; do
+      cat < /tmp/log_pipe_rails | sed 's/^/rails: /'
+      sleep 1
+  done ) &
+
 # Use libmapuid to map ose generate uid for passenger to correct passwd entry
 # otherwise the following error occurs:
 # Cannot checkout session due to Passenger::RuntimeException:
 # Cannot get user database entry for user UID 1000190000; it looks like your system's user database is broken, please fix it.
 export LD_PRELOAD=/usr/local/lib/libmapuid.so
 
-exec /usr/sbin/apachectl -DFOREGROUND 
+exec /usr/sbin/apachectl -DFOREGROUND 2>&1 | sed 's#^#apache stdout/stderr:#'
